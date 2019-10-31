5008b3ebde55275f557efd3d7d1ae63194ba679d
Matt Wright <matthewbwright@gmail.com>
2019-10-31 11:51:35 -0700

    Add project files.

---
+++ RentAFlick/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RentAFlick.Startup))]
+namespace RentAFlick
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
