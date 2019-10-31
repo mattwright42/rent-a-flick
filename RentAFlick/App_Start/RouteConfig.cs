f7e5370b906593f83eacc6eef8c15c6466d68677
Matt Wright <matthewbwright@gmail.com>
2019-10-31 14:15:11 -0700

    Adds some methods for creating routes

--- RentAFlick/App_Start/RouteConfig.cs
+++ RentAFlick/App_Start/RouteConfig.cs
@@ -13,6 +13,16 @@ namespace RentAFlick
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new { controller = "Movies", action = "ByReleaseDate" },
+            //    new { year = @"2015|2016", month = @"\d{2}");
+                //new { year = @"\d{4}", month = @"\d{2}");
+
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- RentAFlick/Controllers/MoviesController.cs
+++ RentAFlick/Controllers/MoviesController.cs
@@ -15,6 +15,35 @@ namespace RentAFlick.Controllers
             var movie = new Movie() { Name = "Shrek" };
 
             return View(movie);
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
