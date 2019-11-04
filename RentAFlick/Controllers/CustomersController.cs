987c790a01f328bf83455fdbd004cd9e51357f22
Matt Wright <matthewbwright@gmail.com>
2019-11-04 11:24:45 -0700

    Updates Customer Controller and View to use the database; adds discount rate to the Customer View

--- RentAFlick/Controllers/CustomersController.cs
+++ RentAFlick/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,17 +10,29 @@ namespace RentAFlick.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
@@ -27,15 +40,15 @@ namespace RentAFlick.Controllers
             return View(customer);
         }
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer {Id = 1, Name = "Frodo Baggins" },
-                new Customer {Id = 2, Name = "Bilbo Baggins" },
-                new Customer {Id = 3, Name = "Gandalf the Grey" },
-                new Customer {Id = 4, Name = "Gollum" }
-            };
-        }
+        //private IEnumerable<Customer> GetCustomers()
+        //{
+        //    return new List<Customer>
+        //    {
+        //        new Customer {Id = 1, Name = "Frodo Baggins" },
+        //        new Customer {Id = 2, Name = "Bilbo Baggins" },
+        //        new Customer {Id = 3, Name = "Gandalf the Grey" },
+        //        new Customer {Id = 4, Name = "Gollum" }
+        //    };
+        //}
     }
 }
