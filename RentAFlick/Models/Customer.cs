8f2a8cac82012eff3986aa931731484d7e772333
Matt Wright <matthewbwright@gmail.com>
2019-11-04 11:02:01 -0700

    Adds Annotations to Customer Name

--- RentAFlick/Models/Customer.cs
+++ RentAFlick/Models/Customer.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace RentAFlick.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToCustomer { get; set; }
         public MembershipType MembershipType { get; set; }
