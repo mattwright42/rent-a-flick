189c736991af86254c2bb6729335a14c8b98df94
Matt Wright <matthewbwright@gmail.com>
2019-11-05 16:08:42 -0700

    Adds birthdate to Customers table; updates the Customer Details view to display customers birthdate and membership type

--- RentAFlick/Models/Customer.cs
+++ RentAFlick/Models/Customer.cs
@@ -16,5 +16,6 @@ namespace RentAFlick.Models
         public bool IsSubscribedToCustomer { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
+        public DateTime? Birthdate { get; set; }
     }
 }
