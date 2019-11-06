4757e7ffeb187543ba717d48be930a3e9192d89e
Matt Wright <matthewbwright@gmail.com>
2019-11-06 11:22:54 -0700

    Adds functionality to edit Customer

--- RentAFlick/ViewModels/NewCustomerViewModel.cs
+++ RentAFlick/ViewModels/NewCustomerViewModel.cs
@@ -6,7 +6,7 @@ using RentAFlick.Models;
 
 namespace RentAFlick.ViewModels
 {
-    public class NewCustomerViewModel
+    public class CustomerFormViewModel
     {
         public IEnumerable<MembershipType> MembershipTypes { get; set; }
         public Customer Customer { get; set; }
