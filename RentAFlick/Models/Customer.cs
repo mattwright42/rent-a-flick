7a0af78b1e4513ae9afa45e1479f6a97f4f66e43
Matt Wright <matthewbwright@gmail.com>
2019-11-06 11:14:05 -0700

    Adds functionality to save a newly created Customer

--- RentAFlick/Models/Customer.cs
+++ RentAFlick/Models/Customer.cs
@@ -13,8 +13,12 @@ namespace RentAFlick.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         public bool IsSubscribedToCustomer { get; set; }
+
         public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
 
         [Display(Name = "Date of Birth")]
