99592ad21c697804641534cfc3acc89d531d8e41
Matt Wright <matthewbwright@gmail.com>
2020-01-06 15:55:24 -0700

    Sets up Auto Mapper

---
+++ RentAFlick/Dtos/CustomerDto.cs
@@ -0,0 +1,26 @@
+﻿using RentAFlick.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RentAFlick.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customer's name.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToCustomer { get; set; }
+
+        //byte = implicitly required
+        public byte MembershipTypeId { get; set; }
+        
+        [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
--- RentAFlick/Global.asax.cs
+++ RentAFlick/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Http;
 using System.Web.Routing;
 using System.Web.Mvc;
 using System.Web.Optimization;
+using AutoMapper;
+using RentAFlick.App_Start;
 
 
 namespace RentAFlick
@@ -14,6 +16,7 @@ namespace RentAFlick
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
             GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
