d016dc3f7cdb89fb4d37cabbc7363111e5839615
Matt Wright <matthewbwright@gmail.com>
2020-01-10 09:26:18 -0700

    Completes addition of Driver's License to Registration

--- RentAFlick/Models/AccountViewModels.cs
+++ RentAFlick/Models/AccountViewModels.cs
@@ -65,6 +65,10 @@ namespace RentAFlick.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "Driver's License")]
+        public string DrivingLicense { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
