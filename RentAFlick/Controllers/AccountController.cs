f421f7db0d5cab3d58649bad296e2fcf9e304746
Matt Wright <matthewbwright@gmail.com>
2020-01-21 17:08:34 -0700

    Adds phone number to registration

--- RentAFlick/Controllers/AccountController.cs
+++ RentAFlick/Controllers/AccountController.cs
@@ -155,7 +155,8 @@ namespace RentAFlick.Controllers
                 var user = new ApplicationUser {
                     UserName = model.Email,
                     Email = model.Email,
-                    DrivingLicense = model.DrivingLicense
+                    DrivingLicense = model.DrivingLicense,
+                    PhoneNumber = model.Phone
                 };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
