485f5c6274e6406eafe2f9fad4d9632de0b95338
Matt Wright <matthewbwright@gmail.com>
2020-02-05 15:13:38 -0700

    Handling edge cases in rentals

--- RentAFlick/Controllers/Api/NewRentalsController.cs
+++ RentAFlick/Controllers/Api/NewRentalsController.cs
@@ -21,14 +21,29 @@ namespace RentAFlick.Controllers.Api
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
+            //Defensive Edge case (with Customers.SingleOrDefault
+            //if (newRental.MovieIds.Count == 0)
+                //return BadRequest("No Movie Ids have been given.");
+
             var customer = _context.Customers.Single(
                 c => c.Id == newRental.CustomerId);
 
+            //Defensive Edge case
+            //if (customer == null)
+            //    return BadRequest("Customer Id is not valid.");
+
             var movies = _context.Movies.Where(
-                m => newRental.MovieIds.Contains(m.Id));
+                m => newRental.MovieIds.Contains(m.Id)).ToList();
+
+            //Defensive Edge case
+            //if (movies.Count != newRental.MovieIds.Count)
+            //    return BadRequest("One or more MovieIds are invalid.");
 
             foreach (var movie in movies)
             {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available.");
+
                 movie.NumberAvailable--;
 
                 var rental = new Rental
